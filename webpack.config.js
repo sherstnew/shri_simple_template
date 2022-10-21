3ef6cde7ce91bfecedccde593511416d78d5e848
denis <88676144+sherstnew@users.noreply.github.com>
2022-10-21 18:05:40 +0300

    homework

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,61 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
+  mode: 'production',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new StatoscopePlugin({
-            saveStatsTo: './stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
+    compress: true,
+    port: 9000,
+  },
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000,
+    },
+  },
+  entry: {
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new StatoscopePlugin({
+      saveReportTo: './report.html',
+      saveStatsTo: './stats.json',
+      saveOnlyStats: false,
+      open: false,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [ // загрузчик для jsx
+      {
+        test: /\.js?$/, // определяем тип файлов
+        exclude: /(node_modules)/, // исключаем из обработки папку node_modules
+        loader: 'babel-loader', // определяем загрузчик
+        options: {
+          presets: ['@babel/preset-react'], // используемые плагины
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-    },
-    module: {
-        rules:[   //загрузчик для jsx
-            {
-                test: /\.js?$/, // определяем тип файлов
-                exclude: /(node_modules)/,  // исключаем из обработки папку node_modules
-                loader: "babel-loader",   // определяем загрузчик
-                options:{
-                    presets:[ "@babel/preset-react"]    // используемые плагины
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
+  },
+  // @TODO optimizations
+  // @TODO lodash treeshaking
+  // @TODO chunk for lodash
+  // @TODO chunk for runtime
+  // @TODO fallback for crypto
 };
 
 module.exports = config;
