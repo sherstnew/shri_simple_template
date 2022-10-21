6ec690d5d2ea31f8afb0a9f1b53d78a758032133
denis <88676144+sherstnew@users.noreply.github.com>
2022-10-21 19:10:27 +0300

    lint clear code

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,12 @@ module.exports = {
     'react',
   ],
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/button-has-type': 'off',
+    'default-param-last': 'off',
+    'react/prop-types': 'off',
+    'no-shadow': 'off',
   },
 };
